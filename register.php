e0c64ae456b4a8ac8853b788c024f44ba6931e99
persian-123 <rohamrahimi22@gmail.com>
2024-11-12 11:42:52 +0000

    username field

--- register.php
+++ register.php
@@ -25,6 +25,15 @@
     </div>
   </div>
   <div class="form-group row">
+            <label for="username" class="col-sm-2 col-form-label text-right">Username</label>
+            <div class="col-sm-10">
+                <input type="text" class="form-control" id="username" name="username" placeholder="Enter username" required>
+                <small id="usernameHelp" class="form-text text-muted">
+                    <span class="text-danger">* Required.</span>
+                </small>
+            </div>
+        </div>
+  <div class="form-group row">
     <label for="email" class="col-sm-2 col-form-label text-right">Email</label>
     <div class="col-sm-10">
       <input type="text" class="form-control" id="email" name="email" placeholder="Email">
