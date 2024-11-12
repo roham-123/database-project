dcf7bcce6e283d0f135bc2f17e971082189f3c22
Aman Ali <sm.aman.ali@gmail.com>
2024-11-12 12:34:30 +0000

    implemented search

--- browse.php
+++ browse.php
@@ -5,14 +5,13 @@
 if (session_status() == PHP_SESSION_NONE) {
   session_start();
 }
+
 if (isset($_SESSION['UserID'])) {
     echo "Logged in as: " . $_SESSION['Name'] . " (Role: " . $_SESSION['Role'] . ")";
 } else {
     echo "Not logged in.";
 }
 ?>
-
-
 
 <div class="container">
 
@@ -33,28 +32,28 @@ if (isset($_SESSION['UserID'])) {
               <i class="fa fa-search"></i>
             </span>
           </div>
-          <input type="text" class="form-control border-left-0" id="keyword" placeholder="Search for anything">
+          <input type="text" class="form-control border-left-0" id="keyword" name="keyword" placeholder="Search for anything" value="<?php echo isset($_GET['keyword']) ? htmlspecialchars($_GET['keyword']) : ''; ?>">
         </div>
       </div>
     </div>
     <div class="col-md-3 pr-0">
       <div class="form-group">
         <label for="cat" class="sr-only">Search within:</label>
-        <select class="form-control" id="cat">
+        <select class="form-control" id="cat" name="cat">
           <option selected value="all">All categories</option>
-          <option value="fill">Fill me in</option>
-          <option value="with">with options</option>
-          <option value="populated">populated from a database?</option>
+          <option value="Electronics" <?php echo isset($_GET['cat']) && $_GET['cat'] == 'Electronics' ? 'selected' : ''; ?>>Electronics</option>
+          <option value="Furniture" <?php echo isset($_GET['cat']) && $_GET['cat'] == 'Furniture' ? 'selected' : ''; ?>>Furniture</option>
+          <option value="Clothing" <?php echo isset($_GET['cat']) && $_GET['cat'] == 'Clothing' ? 'selected' : ''; ?>>Clothing</option>
         </select>
       </div>
     </div>
     <div class="col-md-3 pr-0">
       <div class="form-inline">
         <label class="mx-2" for="order_by">Sort by:</label>
-        <select class="form-control" id="order_by">
-          <option selected value="pricelow">Price (low to high)</option>
-          <option value="pricehigh">Price (high to low)</option>
-          <option value="date">Soonest expiry</option>
+        <select class="form-control" id="order_by" name="order_by">
+          <option selected value="pricelow" <?php echo isset($_GET['order_by']) && $_GET['order_by'] == 'pricelow' ? 'selected' : ''; ?>>Price (low to high)</option>
+          <option value="pricehigh" <?php echo isset($_GET['order_by']) && $_GET['order_by'] == 'pricehigh' ? 'selected' : ''; ?>>Price (high to low)</option>
+          <option value="date" <?php echo isset($_GET['order_by']) && $_GET['order_by'] == 'date' ? 'selected' : ''; ?>>Soonest expiry</option>
         </select>
       </div>
     </div>
@@ -64,7 +63,6 @@ if (isset($_SESSION['UserID'])) {
   </div>
 </form>
 </div> <!-- end search specs bar -->
-
 
 </div>
 
@@ -90,23 +88,10 @@ if (isset($_SESSION['UserID'])) {
   else {
     $ordering = $_GET['order_by'];
   }
-  
-  if (!isset($_GET['page'])) {
-    $curr_page = 1;
-  }
-  else {
-    $curr_page = $_GET['page'];
-  }
 
   /* TODO: Use above values to construct a query. Use this query to 
      retrieve data from the database. (If there is no form data entered,
      decide on appropriate default value/default query to make. */
-  
-  /* For the purposes of pagination, it would also be helpful to know the
-     total number of results that satisfy the above query */
-  $num_results = 96; // TODO: Calculate me for real
-  $results_per_page = 10;
-  $max_page = ceil($num_results / $results_per_page);
 ?>
 
 <div class="container mt-5">
@@ -127,9 +112,9 @@ $ordering = isset($_GET['order_by']) ? $_GET['order_by'] : 'date';
 // Set up the base SQL query to fetch auctions
 $sql = "SELECT AuctionID, ItemName, Description, StartPrice, ReservePrice, EndDate FROM Auction WHERE EndDate > NOW()";
 
-// Add filters to the SQL query if provided
+// Add filter for keyword on ItemName only
 if (!empty($keyword)) {
-    $sql .= " AND (ItemName LIKE ? OR Description LIKE ?)";
+    $sql .= " AND ItemName LIKE ?";
     $keyword_param = "%" . $keyword . "%";
 }
 if ($category !== 'all') {
@@ -152,9 +137,9 @@ switch ($ordering) {
 // Prepare and execute the query
 $stmt = $conn->prepare($sql);
 if (!empty($keyword) && $category !== 'all') {
-    $stmt->bind_param("sss", $keyword_param, $keyword_param, $category);
+    $stmt->bind_param("ss", $keyword_param, $category);
 } elseif (!empty($keyword)) {
-    $stmt->bind_param("ss", $keyword_param, $keyword_param);
+    $stmt->bind_param("s", $keyword_param);
 } elseif ($category !== 'all') {
     $stmt->bind_param("s", $category);
 }
@@ -190,69 +175,6 @@ $stmt->close();
 
 </ul>
 
-<!-- Pagination for results listings -->
-<nav aria-label="Search results pages" class="mt-5">
-  <ul class="pagination justify-content-center">
-  
-<?php
-
-  // Copy any currently-set GET variables to the URL.
-  $querystring = "";
-  foreach ($_GET as $key => $value) {
-    if ($key != "page") {
-      $querystring .= "$key=$value&amp;";
-    }
-  }
-  
-  $high_page_boost = max(3 - $curr_page, 0);
-  $low_page_boost = max(2 - ($max_page - $curr_page), 0);
-  $low_page = max(1, $curr_page - 2 - $low_page_boost);
-  $high_page = min($max_page, $curr_page + 2 + $high_page_boost);
-  
-  if ($curr_page != 1) {
-    echo('
-    <li class="page-item">
-      <a class="page-link" href="browse.php?' . $querystring . 'page=' . ($curr_page - 1) . '" aria-label="Previous">
-        <span aria-hidden="true"><i class="fa fa-arrow-left"></i></span>
-        <span class="sr-only">Previous</span>
-      </a>
-    </li>');
-  }
-    
-  for ($i = $low_page; $i <= $high_page; $i++) {
-    if ($i == $curr_page) {
-      // Highlight the link
-      echo('
-    <li class="page-item active">');
-    }
-    else {
-      // Non-highlighted link
-      echo('
-    <li class="page-item">');
-    }
-    
-    // Do this in any case
-    echo('
-      <a class="page-link" href="browse.php?' . $querystring . 'page=' . $i . '">' . $i . '</a>
-    </li>');
-  }
-  
-  if ($curr_page != $max_page) {
-    echo('
-    <li class="page-item">
-      <a class="page-link" href="browse.php?' . $querystring . 'page=' . ($curr_page + 1) . '" aria-label="Next">
-        <span aria-hidden="true"><i class="fa fa-arrow-right"></i></span>
-        <span class="sr-only">Next</span>
-      </a>
-    </li>');
-  }
-?>
-
-  </ul>
-</nav>
-
-
 </div>
-
 
 <?php include_once("footer.php")?>
